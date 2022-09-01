2e3d6e399126004ee0e6f0895d107a06e8177c08
Xer0X <57611392+dr-dba@users.noreply.github.com>
2022-09-01 22:19:16 +0300

    [code-prettify@Xer0X]

--- far/scrbuf.cpp
+++ far/scrbuf.cpp
@@ -84,11 +84,12 @@ static void invalidate_broken_pairs_in_cache(matrix<FAR_CHAR_INFO>const& Buf, ma
 	const auto
 		IsLeft = !Point.x && Where.left,
 		IsRight = Point.x == Where.width() - 1 && Where.right != ScrX;
-	if (!IsLeft && !IsRight)
+	if(!IsLeft
+	&& !IsRight)
 		return;
-	const auto X1X2 = IsLeft?
-		std::pair{ Where.left - 1, Where.left      } :
-		std::pair{ Where.right,    Where.right + 1 };
+	const auto X1X2 = IsLeft
+		? std::pair{ Where.left - 1,Where.left      }
+		: std::pair{ Where.right,	Where.right + 1 };
 	const auto
 		BufRowData = Buf[Where.top + Point.y],
 		ShadowRowData = Shadow[Where.top + Point.y];
@@ -262,9 +263,9 @@ void ScreenBuf::ApplyShadow(rectangle Where, bool const IsLegacy)
 			// This piece is for usage with repeated Message() calls.
 			// It generates a stable shadow that does not fade to black when reapplied over and over.
 			// We really, really should ditch the Message pattern.
-			Element.Attributes.IsBgIndex()?
-				colors::set_index_value(Element.Attributes.BackgroundColor, F_BLACK) :
-				colors::set_color_value(Element.Attributes.BackgroundColor, 0);
+			Element.Attributes.IsBgIndex()
+				? colors::set_index_value(Element.Attributes.BackgroundColor, F_BLACK)
+				: colors::set_color_value(Element.Attributes.BackgroundColor, 0	);
 			if (Element.Attributes.IsFgIndex())
 			{
 				const auto Mask = FOREGROUND_INTENSITY;
